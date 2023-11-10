002c5cf9997acb09db05800a016e22a172ad4aa8
Bunatz <nathaniel.kurr@fau.de>
2023-11-10 08:48:59 +0100

    code modification, Comments Docstrings for better readable, also error handling for buggfixing code

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,108 @@
 import random
 
 
-def function_A(min, max):
+def create_rand_int(min_value, max_value):
     """
-    Random integer.
+    Function:
+    It creates an random integer inbetween 
+    the range of two Parameter(min_value, max_value).
+    
+    Parameter:
+    min_value(int): Minimum integer value
+    max_value(int): Maximum integer value    
+    
+    Return:
+    Returns one integer number
+    
     """
-    return random.randint(min, max)
+    return random.randint(min_value, max_value)
 
 
-def function_B():
+def create_rand_operator():
+    """
+    Function:
+    It creates an random operator (+, -, *)
+    
+    Paramter:
+    --non--
+    
+    Return:
+    Returns one operator 
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def cal_result_numbers(number1, number2, operator):
+    """
+    Function:
+    It calculates the result between two numbers(number1, number2)
+    and an operator (+, -, *) 
+    
+    Paramter:
+    number1 (int): First number
+    number2 (int): second number
+    operator(str): Operator for the calcualtion
+    
+    Return:
+    Returns the result and the calculation as a sting
+
+    """
+    calculation = f"{number1} {operator} {number2}"
+    
+    if operator == '+': 
+        result = number1 + number2
+        
+    elif operator == '-': 
+        result = number1 - number2
+        
+    else: 
+        result = number1 * number2
+        
+    return calculation, result
+
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    """
+    Function: 
+    It connectes three function create_rand_int, 
+    create_rand_operator, cal_result_numbers. 
+    
+    Return: 
+    The quiz gives out three random calculation and ask for the answer.
+    It returns the reached score after finishing   
+    """
+    start_score = 0
+    number_question = 3 #how many calculation are required
 
     print("Welcome to the Math Quiz Game!")
-    print("You will be presented with math problems, and you need to provide the correct answers.")
+    print("You will be presented with math problems, and you need to provide \
+          the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(number_question):
+        num1 = create_rand_int(1, 10); 
+        num2 = create_rand_int(1, 5); 
+        rand_op = create_rand_operator()
+        problem, answer = cal_result_numbers(num1, num2, rand_op)
+        
+        print(f"\nQuestion: {problem}")
+        
+      
+        try: 
+            useranswer = input("Your answer: ")
+            value_useranswer = int(useranswer)  #Try to convert the useranswer into integer number
+            
+        except ValueError:
+            print("please insert integer number") # Error if the input isnt a number
+        
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
-
-        if useranswer == ANSWER:
+        if value_useranswer == answer:
             print("Correct! You earned a point.")
-            s += -(-1)
+            start_score += +1
+            
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            print(f"Wrong answer. The correct answer is {answer}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {start_score}/{number_question}") #shows the Score at the end of the game
 
 if __name__ == "__main__":
     math_quiz()
